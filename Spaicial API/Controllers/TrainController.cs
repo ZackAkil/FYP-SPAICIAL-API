d90216e873f8abcc4bd8e385e195f8fdfdcfd9ad
ZackAkil <zackakil94@gmail.com>
2016-03-30 15:09:46 +0100

    added base structure of training data building code

--- Spaicial API/Controllers/TrainController.cs
+++ Spaicial API/Controllers/TrainController.cs
@@ -17,31 +17,70 @@ namespace Spaicial_API.Controllers
 
         // GET: api/ScoutData/5
         [ResponseType(typeof(string))]
-        public async Task<IHttpActionResult> GetTrainZone(int id,string dataSubject)
+        public async Task<IHttpActionResult> GetTrainZone(int id, string dataSubject)
         {
             Zone zoneToTrain = await db.Zone.FindAsync(id);
-
-            int dataSubjectId = db.DataSubject.Where(d => d.label == dataSubject).First().dataSubjectId;
-
-            //get scout data that is in the area of the zone and has the data subject we want to predict
-            var scoutData = db.ScoutData.Where(s => (s.ScoutDataPart.Where(p=> p.dataSubjectId == dataSubjectId)).Count()>0)
-                .Where(s => s.locationPoint.Intersects(zoneToTrain.locationArea));
-
-            //get station that are mention in prediction
-            var stations = zoneToTrain.Feature1.Where(f => f.predictedDataSubjectId == dataSubjectId)
-                .Select(f => f.Zone).Distinct().ToList();
-
-            //get data subjects that are mentioned in the prediction
-            var dataSubjects = zoneToTrain.Feature1.Where(f => f.predictedDataSubjectId == dataSubjectId)
-                .Select(f => f.DataSubject).Distinct();
-
-            //Console.Write(stationData.ToArray().to);
-            var count = scoutData.Count();
 
             if (zoneToTrain == null)
             {
                 return NotFound();
             }
+
+            int predictedDataSubjectId = db.DataSubject.Where(d => d.label == dataSubject).First().dataSubjectId;
+
+            var featuresToTrain = zoneToTrain.Feature1.Where(f => f.predictedDataSubjectId == predictedDataSubjectId);
+           
+            //get scout data that is in the area of the zone and has the data subject we want to predict
+            var scoutData = db.ScoutData.Where(s => (s.ScoutDataPart.Where(p => p.dataSubjectId == predictedDataSubjectId)).Count() > 0)
+                .Where(s => s.locationPoint.Intersects(zoneToTrain.locationArea));
+
+
+            //join scout data with station data that has same recorded time
+            var scoutDataAndStationData =
+                from sc in scoutData
+                join st in db.StationData on sc.dateTimeCollected equals st.dateTimeCollected
+                select new { scout = sc, station = st };
+
+            //get stations that are mentioned in prediction
+            var stationsMentioned = zoneToTrain.Feature1.Where(f => f.predictedDataSubjectId == predictedDataSubjectId)
+                .Select(f => f.Zone).Distinct();
+            
+            //get data subjects that are mentioned in the prediction
+            var dataSubjectsMentioned = zoneToTrain.Feature1.Where(f => f.predictedDataSubjectId == predictedDataSubjectId)
+                .Select(f => f.DataSubject).Distinct();
+
+            //create jagged array of same size as all features that will be optimized (+1 for bias)
+            double[][] trainingData = new double[featuresToTrain.Count() + 1][];
+
+            //build up training data
+            foreach (var scoutDataItem in scoutData)
+            {
+                foreach (var stationMentionedItem in stationsMentioned)
+                {
+                    foreach (var dataSubjectItem in dataSubjectsMentioned)
+                    {
+                        //if feature with current station and data subject is used in prediction
+                        if(featuresToTrain.Where(f => (f.sourceZoneId == stationMentionedItem.zoneId)
+                            &(f.sourceDataSubjectId == dataSubjectItem.dataSubjectId)).Any()){
+
+                            var featureDataQuery = db.StationDataPart.Where(s => (s.StationData.zoneId == stationMentionedItem.zoneId)
+                           & (s.dataSubjectId == dataSubjectItem.dataSubjectId));
+
+                        }
+
+                    }
+
+                }
+            }
+
+
+            //var queryForDataSubject = db.StationDataPart.Where(s => s.dataSubjectId == 1);
+            //var queryForDataSubject2 = db.StationDataPart.Where(s => s.dataSubjectId == 2);
+
+            //var innerJoinQuery =
+            //from d in queryForDataSubject
+            //join d2 in queryForDataSubject2 on d.stationDataId equals d2.stationDataId
+            //select new { stationDataId = d.stationDataId, windSpeed = d.dataValue, windDirection = d2.dataValue, date = d.StationData.dateTimeCollected }; //produces flat sequence
 
             return Ok("hello");
         }
