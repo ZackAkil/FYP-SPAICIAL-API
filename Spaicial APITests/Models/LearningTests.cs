2528dd3cfa3e4b89baaf04532d2d9ddb4e31c434
ZackAkil <zackakil94@gmail.com>
2016-04-03 11:49:47 +0100

    added unit test for learning class

---
+++ Spaicial APITests/Models/LearningTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spaicial_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spaicial_API.Models.Tests
+{
+    [TestClass()]
+    public class LearningTests
+    {
+        [TestMethod()]
+        public void PredictFunctionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LearnTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LearnTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
