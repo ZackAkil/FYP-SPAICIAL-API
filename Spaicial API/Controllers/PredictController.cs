0087f6d66c83b80f19ad2ee51308ddc28988c609
ZackAkil <zackakil94@gmail.com>
2016-04-05 12:35:44 +0100

    added api key check to prediction controller

--- Spaicial API/Controllers/PredictController.cs
+++ Spaicial API/Controllers/PredictController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
@@ -22,11 +23,11 @@ namespace Spaicial_API.Controllers
         }
 
         private spaicial_dbEntities db = new spaicial_dbEntities();
-
-        // GET: api/Predict?id=3&dataSubject=wind%20speed
+        // GET: api/Predict?id=3&dataSubject=wind%20speed&apiKey=abcdefg
         [ResponseType(typeof(PredictionResponse))]
-        public async Task<IHttpActionResult> GetPrediction(int id, string dataSubject)
+        public async Task<IHttpActionResult> GetPrediction(int id, string dataSubject, string apiKey)
         {
+            CheckApiKey(apiKey);
 
             Zone zoneToTrain = await db.Zone.FindAsync(id);
             if (zoneToTrain == null)
@@ -40,6 +41,37 @@ namespace Spaicial_API.Controllers
             predictor.GetPrediction();
            
             return Ok(new PredictionResponse { value = predictor.predictionValue, latestDataUsed = predictor.predictionAge });
+        }
+
+        private void CheckApiKey(string apiKey)
+        {
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                var response = new HttpResponseException(new HttpResponseMessage(HttpStatusCode.Unauthorized)
+                {
+                    Content = new StringContent("No API key"),
+                    ReasonPhrase = "The API key was not supplied."
+                });
+
+                var challenge = new AuthenticationHeaderValue("valid_ApiKey_required");
+                response.Response.Headers.WwwAuthenticate.Add(challenge);
+
+                throw response;
+
+            }
+            else if (!db.ApiKey.Any(a => a.keyValue == apiKey))
+            {
+                var response = new HttpResponseException(new HttpResponseMessage(HttpStatusCode.Unauthorized)
+                {
+                    Content = new StringContent("Invalid API key"),
+                    ReasonPhrase = "The API key used does not exist in the system."
+                });
+
+                var challenge = new AuthenticationHeaderValue("valid_ApiKey_required");
+                response.Response.Headers.WwwAuthenticate.Add(challenge);
+
+                throw response;
+            }
         }
 
 
